a52d8769d4737d17b223bd706a272778a6504b5f
Hirudika-Dilshan <hirudikadilshanuni@gmail.com>
2025-11-14 16:56:09 +0530

    Update EventController.java

--- src/main/java/com/eventstream/ingestion_service/controller/EventController.java
+++ src/main/java/com/eventstream/ingestion_service/controller/EventController.java
@@ -12,13 +12,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // මේක 'REST Controller' එකක් කියලා Spring වලට කියනවා
-@RequestMapping("/api/v1") // මේ controller එකේ තියෙන எல்லா endpoints වලටම common prefix එක
+@RestController 
+@RequestMapping("/api/v1") 
 public class EventController {
 
     private static final Logger log = LoggerFactory.getLogger(EventController.class);
 
-    // 'Service' layer එක 'inject' කරගන්නවා
+    
     private final EventService eventService;
 
     public EventController(EventService eventService) {
@@ -29,18 +29,15 @@ public class EventController {
     @PostMapping("/event")
     public ResponseEntity<Void> handleEvent(
 
-            // @Valid: DTO එකේ තියෙන validation rules (@NotBlank) check කරන්න කියනවා
-            // @RequestBody: JSON payload එක EventRequest object එකට convert කරන්න කියනවා
+            
             @Valid @RequestBody EventRequest eventRequest) {
 
         log.info("Received event ingestion request: {}", eventRequest.eventType());
 
-        // 1. Data එක Service එකට pass කරනවා
-        // මේ method එක ඉක්මනට return වෙනවා (async නිසා)
+        
         eventService.processEvent(eventRequest);
 
-        // 2. Assignment requirement: HTTP 202 Accepted return කරනවා
-        // "බාරගත්තා, process කරන්නම්"
+        
         return ResponseEntity.accepted().build();
     }
 }
