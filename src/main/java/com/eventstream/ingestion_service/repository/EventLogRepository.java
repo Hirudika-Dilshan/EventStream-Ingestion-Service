73b199c5c0df0ed2bf21355eea34bec85cf046b9
Hirudika-Dilshan <hirudikadilshanuni@gmail.com>
2025-11-14 16:57:52 +0530

    Update EventLogRepository.java

--- src/main/java/com/eventstream/ingestion_service/repository/EventLogRepository.java
+++ src/main/java/com/eventstream/ingestion_service/repository/EventLogRepository.java
@@ -4,12 +4,8 @@ import com.eventstream.ingestion_service.entity.EventLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository // මේක 'Repository' component එකක් කියලා Spring වලට කියනවා
+@Repository 
 public interface EventLogRepository extends JpaRepository<EventLog, Long> {
 
-    // මෙතන කිසිම code එකක් ලියන්න ඕනේ නෑ!
-    // 'JpaRepository' එක 'extend' කරපුවාම
-    // .save(..), .findById(..), .findAll() වගේ
-    // database methods ඔක්කොම අපිට automatically හම්බවෙනවා.
-
+    
 }
